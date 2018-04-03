800810169522628856e253b5ecd00de1a5a8579d
hetman <yv.sujim@gmail.com>
2018-04-03 16:32:46 +0300

    del

--- src/Main.java
+++ src/Main.java
@@ -9,19 +9,7 @@ public class Main {
 <head>
 <title>Hello gt</title>
         + <script>console.log("hello git!")</script>
-        System.out.println("Hello World!");
-<head>
-	<title>Hello gt<title>
-        + <script>console.log("hello git!")</script>
-</head>
-        git push [--all | --mirror | --tags] [--follow-tags] [--atomic] [-n | --dry-run] [--receive-pack=<git-receive-pack>]
-        [--repo=<repository>] [-f | --force] [-d | --delete] [--prune] [-v | --verbose]
-        [-u | --set-upstream] [--push-option=<string>]
-        [--[no-]signed|--signed=(true|false|if-asked)]
-        [--force-with-lease[=<refname>[:<expect>]]]
-        [--no-verify] [<repository> [<refspec>…​]]
 
-    git fetch              # update 'master' from remote
-    git tag base master    # mark our base point
-    git rebase -i master   # rewrite some commits
     git push --force-with-lease=master:base master:master
+    https://github.com/Sujim/qwerty.git
+            53ce7d083f0d4d5014ba7af1ae26a022a6a0fcca
