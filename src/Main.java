9a8bdc8c0955dcbdd9af50a34850f5e04050eb45
hetman <yv.sujim@gmail.com>
2018-04-03 15:02:14 +0300

    new projects

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hello World!1");
+        System.out.println("Hello World!4");
+
+        System.out.println("Hello77 World!5");
+
+        System.out.println("Hello World!6");
+
+        System.out.println("Hello World!8");
+        System.out.println("Hello World!69");
+
+        System.out.println("Hello World!90");
+        System.out.println("Hello World!869")
+
+
+        ddddddd;
+    }
+}
