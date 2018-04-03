6adbe828efcbadb795e6437bfd0241c76e449c87
hetman <yv.sujim@gmail.com>
2018-04-03 15:25:09 +0300

    del row

--- src/Main.java
+++ src/Main.java
@@ -2,40 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("Hello World!1");
-        System.out.println("Hello World!4");
-
-        System.out.println("Hello77 World!5");
-
-        System.out.println("Hello World!6");
-
-        System.out.println("Hello World!8");
-        System.out.println("Hello World!69");
-
-        System.out.println("Hello World!90");
-        System.out.println("Hello World!869")
-
-
-        ddddddd;
-
-}
 <head>
-<title>Hello gt</title>
+	<title>Hello gt<title>
         + <script>console.log("hello git!")</script>
 </head>
-<html>
-<head>
-<title>Hello gt</title>
-</head>
-<body>
-    First git repo
-</body>
-</html>
-<head>
-<title>Hello gt<title>
-	+ <script>console.log("hello git!")</script>
-</head>
-51ee36c4e6aae1c25b0a089e3928e8b565dee90a51ee36c4e6aae1c25b0
-    a089e3928e8b565dee90a51ee36c4e6aae1c25b0a089e3928e
-    8b565dee90a51ee36c4e6aae1c25b0a089e3928e8b565dee90a51
-    ee36c4e6aae1c25b0a089e3928e8b565dee90a
