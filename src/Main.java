5080fb44e2c47f355d8b07b2a344a9d89a162bb5
hetman <yv.sujim@gmail.com>
2018-04-03 17:49:53 +0300

    Revert "2"
    This reverts commit 0efe654

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,15 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        git fetch              # update 'master' from remote
+        git tag base master    # mark our base point
+        git rebase -i master   # rewrite some commits
+        git push --force-with-lease=master:base master:master
+}
 <head>
-	<title>Hello gt<title>
+<title>Hello gt</title>
         + <script>console.log("hello git!")</script>
-</head>
+
+    git push --force-with-lease=master:base master:master
+    https://github.com/Sujim/qwerty.git
+            53ce7d083f0d4d5014ba7af1ae26a022a6a0fccabdfh dfghjxtfhbsvgsegesg
